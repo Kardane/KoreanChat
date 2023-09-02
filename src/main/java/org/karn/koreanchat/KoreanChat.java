828d6f78a77a6c637b7becfdeeb6d99e61d1dd16
qkrehf2 <64183602+qkrehf2@users.noreply.github.com>
2023-09-02 22:19:11 +0900

    Removed :)

--- src/main/java/org/karn/koreanchat/KoreanChat.java
+++ src/main/java/org/karn/koreanchat/KoreanChat.java
@@ -2,24 +2,13 @@ package org.karn.koreanchat;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.message.v1.ServerMessageDecoratorEvent;
-import net.minecraft.text.Text;
 import org.karn.koreanchat.command.ChatToggle;
-import org.karn.koreanchat.util.ExceptionStringUtil;
-
-import java.util.concurrent.CompletableFuture;
 
 public class KoreanChat implements ModInitializer {
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, commandRegistryAccess, ignored1) -> {
             ChatToggle.register(dispatcher);
-        });
-
-        ServerMessageDecoratorEvent.EVENT.register(ServerMessageDecoratorEvent.CONTENT_PHASE, (sender, message) -> {
-            Text replaced = message.copy().append(" :)");
-            return CompletableFuture.completedFuture(replaced);
         });
     }
 
